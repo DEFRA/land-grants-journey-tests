cb56b9abae8ad16c0fd32b5b5268ede8f6d7f317
Mayank Solanki <manky18@gmail.com>
2025-06-09 11:48:49 +0100

    Adding negative tests for CMOR1 action

--- test/page-objects/actions.page.js
+++ test/page-objects/actions.page.js
@@ -2,10 +2,15 @@ import { Page } from 'page-objects/page'
 
 class ActionsPage extends Page {
   async selectRequiredAction(actionName, quantity) {
-    const checkbox = await $(`input[type='checkbox'][value='${actionName}']`)
+    const checkbox = await $(`input[id='${actionName}']`)
     await checkbox.click()
     const quantityInput = await $(`input[name='qty-${actionName}']`)
     await quantityInput.setValue(quantity)
+  }
+
+  async getErrorMessage() {
+    const errorElement = await $('.govuk-list.govuk-error-summary__list')
+    return await errorElement.getText()
   }
 }
 
