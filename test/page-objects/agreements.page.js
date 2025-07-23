7b712570fc6492eaea021f12584cb10125512ef3
Arijit Ghosh <arijit.ghosh@equalexperts.com>
2025-07-23 12:28:50 +0100

    FRPSSFI-87- New Scenario to Test Proxy changes

--- test/page-objects/agreements.page.js
+++
@@ -1,9 +0,0 @@
-import { Page } from 'page-objects/page'
-
-class AgreementsPage extends Page {
-  open() {
-    return super.open('/agreements/SFI123456789')
-  }
-}
-
-export default new AgreementsPage()
--- test/specs/agreements_validations.js
+++ test/specs/agreements_validations.js
@@ -1,27 +1,31 @@
 import { expect } from '@wdio/globals'
-import AgreementsPage from 'page-objects/agreements.page.js'
+import { ReviewOfferPage } from 'page-objects/review-offer.page.js'
+import { AcceptYourOfferPage } from 'page-objects/accept-your-offer.page.js'
+const reviewOfferPage = new ReviewOfferPage()
+const acceptYourOfferPage = new AcceptYourOfferPage()
+const ACCEPT_OFFER_TITLE = 'Accept offer'
+const ACCEPT_OFFER_HEADER = 'Accept your offer'
+const REVIEW_OFFER_TITLE = 'Review funding offer'
+const REVIEW_OFFER_HEADER = 'Review your funding offer'
 
-describe('Display Agreements - Offer Page for farmer', () => {
-  // Context: Farmer is eligible for funding
+describe('Display Agreements - Review Offer Page and AcceptYourOfferPage', () => {
   describe('Given farmer has received offer notification', () => {
     describe('When farmer clicks on the offer link', () => {
       beforeEach(async () => {
-        await AgreementsPage.open()
+        await reviewOfferPage.open()
       })
 
-      it('shows the offer page', async () => {
-        await expect(browser).toHaveTitle(
-          // TODO:
-          'Something went wrong | Manage land-based actions'
+      it('shows the Review Offer Page', async () => {
+        await expect(browser).toHaveTitle(REVIEW_OFFER_TITLE)
+        expect(await reviewOfferPage.getPageHeader()).toBe(REVIEW_OFFER_HEADER)
+      })
+
+      it('shows the Accept Your Offer Page', async () => {
+        await reviewOfferPage.selectContinue()
+        await expect(browser).toHaveTitle(ACCEPT_OFFER_TITLE)
+        expect(await acceptYourOfferPage.getPageHeader()).toBe(
+          ACCEPT_OFFER_HEADER
         )
-      })
-
-      it('has different sections', async () => {
-        // TODO:
-      })
-
-      it('shows the accept button', async () => {
-        // TODO:
       })
     })
   })
