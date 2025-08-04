2a30081133c9c0b1f0718b037984a0da273581c0
Mayank Solanki <manky18@gmail.com>
2025-08-04 16:50:21 +0100

    Update to ensure the checkbox is selected and to fix the area value

--- test/page-objects/actions.page.js
+++ test/page-objects/actions.page.js
@@ -3,7 +3,10 @@ import { Page } from 'page-objects/page'
 class ActionsPage extends Page {
   async selectRequiredAction(actionName, quantity) {
     const checkbox = await $(`input[type='checkbox'][value='${actionName}']`)
-    await checkbox.click()
+    const isChecked = await checkbox.isSelected()
+    if (!isChecked) {
+      await checkbox.click()
+    }
     const quantityInput = await $(`input[name='qty-${actionName}']`)
     await quantityInput.setValue(quantity)
   }
