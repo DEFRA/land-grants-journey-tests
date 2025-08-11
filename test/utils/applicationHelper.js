b63e75dcaeb44f059d4ac65e08c67bfef16278f7
Narahari Miriyala <miriyalanarahari@gmail.com>
2025-08-11 15:49:57 +0100

    Update tests to handle save and return incomplete previous applications

---
+++ test/utils/applicationHelper.js
@@ -0,0 +1,54 @@
+import HomePage from 'page-objects/home.page.js'
+import knockoutQuestionsPage from 'page-objects/knockout.questions.page.js'
+import LandParcelsPage from 'page-objects/land.parcels.page.js'
+import AddMoreActionsPage from 'page-objects/add.more.actions.page.js'
+import ConfirmYourDetailsPage from 'page-objects/confirm.your.details.page.js'
+import CheckYourAnswersPage from '../page-objects/check.your.answers.page'
+import SubmitYourApplicationPage from '../page-objects/submit.your.application.page'
+import ConfirmYouWillBeEligiblePage from '../page-objects/confirm.you.will.be.eligible.page'
+
+class ApplicationHelper {
+  async submitPreviousInCompleteApplication(parcel, actionName) {
+    // Navigate to the home page and start a new application
+    await HomePage.open()
+    await HomePage.clickButton('Start now')
+    await ConfirmYourDetailsPage.clickButton('Continue')
+    await ConfirmYouWillBeEligiblePage.clickButton('Continue')
+
+    // check if the radio button is selected
+    const radioButton = await $(`input[type='radio'][value='true']`)
+    const isSelected = await radioButton.isSelected()
+    if (!isSelected) {
+      return
+    }
+
+    // If radio button is selected there is a previous incomplete application, submit the application to clear the cache
+    await knockoutQuestionsPage.clickButton('Continue')
+
+    // Select land parcel and click continue button on the land parcels page
+    await LandParcelsPage.selectRadioButtonByValue(parcel)
+    await LandParcelsPage.clickButton('Continue')
+
+    // Click the continue button on the add actions page
+    const checkbox = await $(`input[type='checkbox'][value='${actionName}']`)
+    const isChecked = await checkbox.isSelected()
+    if (!isChecked) {
+      await checkbox.click()
+    }
+    await LandParcelsPage.clickButton('Continue')
+
+    // Click the continue button on the add more actions page
+    await AddMoreActionsPage.selectRadioButtonByValue('false')
+    await AddMoreActionsPage.clickButton('Continue')
+
+    // Click the continue button on the check your answers page
+    const continueButton = await CheckYourAnswersPage.continueButton()
+    continueButton.click()
+
+    // Click the continue button on the submit your application page
+    const submitButton = await SubmitYourApplicationPage.continueButton()
+    submitButton.click()
+  }
+}
+
+export default new ApplicationHelper()
