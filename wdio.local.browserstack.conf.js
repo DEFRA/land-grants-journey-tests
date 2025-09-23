44362fc584e7ebfaf4af62b64c0c396a2b316712
Mayank Solanki <manky18@gmail.com>
2025-09-23 10:49:28 +0100

    add BrowserStack integration capability

---
+++ wdio.local.browserstack.conf.js
@@ -0,0 +1,83 @@
+import allure from 'allure-commandline'
+import { browserStackCapabilities } from './wdio.browserstack.capabilities.js'
+
+export const config = {
+  user: process.env.BROWSERSTACK_USERNAME,
+  key: process.env.BROWSERSTACK_KEY,
+  // Local
+  baseUrl: 'http://localhost:3000',
+  runner: 'local',
+  specs: ['./test/specs/**/*.js'],
+  exclude: [],
+  maxInstances: 10,
+  capabilities: browserStackCapabilities,
+  services: [
+    [
+      'browserstack',
+      {
+        testObservability: true,
+        testObservabilityOptions: {
+          user: process.env.BROWSERSTACK_USERNAME,
+          key: process.env.BROWSERSTACK_KEY,
+          projectName: 'land-grants-journey-tests',
+          buildName: `land-grants-journey-tests-local`
+        },
+        acceptInsecureCerts: true,
+        forceLocal: true,
+        browserstackLocal: true
+      }
+    ]
+  ],
+  logLevel: 'info',
+  bail: 1,
+  waitforTimeout: 10000,
+  waitforInterval: 200,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  framework: 'mocha',
+  reporters: [
+    'spec',
+    [
+      'allure',
+      {
+        outputDir: 'allure-results'
+      }
+    ]
+  ],
+  mochaOpts: {
+    ui: 'bdd',
+    timeout: 600000
+  },
+  afterTest: async function (
+    test,
+    context,
+    { error, result, duration, passed, retries }
+  ) {
+    await browser.takeScreenshot()
+
+    if (error) {
+      browser.executeScript(
+        'browserstack_executor: {"action": "setSessionStatus", "arguments": {"status":"failed","reason": "At least 1 assertion failed"}}'
+      )
+    }
+  },
+  onComplete: function (exitCode, config, capabilities, results) {
+    const reportError = new Error('Could not generate Allure report')
+    const generation = allure(['generate', 'allure-results', '--clean'])
+
+    return new Promise((resolve, reject) => {
+      const generationTimeout = setTimeout(() => reject(reportError), 60000)
+
+      generation.on('exit', function (exitCode) {
+        clearTimeout(generationTimeout)
+
+        if (exitCode !== 0) {
+          return reject(reportError)
+        }
+
+        allure(['open'])
+        resolve()
+      })
+    })
+  }
+}
