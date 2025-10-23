ea58b1061c09d983de4c27e2f979331cc2e607d1
Mayank Solanki <manky18@gmail.com>
2025-10-23 14:09:26 +0100

    add capabilities for multiples browsers / os

--- wdio.browserstack.capabilities.js
+++ wdio.browserstack.capabilities.js
@@ -12,32 +12,123 @@ export const browserStackCapabilities = [
       os: 'Windows',
       osVersion: '11'
     }
-  }
-  // {
-  //   browserName: 'Edge',
-  //   'wdio-ics:options': {
-  //     logName: 'win-edge'
-  //   },
-  //   'bstack:options': {
-  //     idleTimeout: 300,
-  //     resolution: '1920x1080',
-  //     browserVersion: 'latest',
-  //     os: 'Windows',
-  //     osVersion: '11'
-  //   }
-  // },
+  },
+  {
+    browserName: 'Edge',
+    'wdio-ics:options': {
+      logName: 'win-edge'
+    },
+    'bstack:options': {
+      idleTimeout: 300,
+      resolution: '1920x1080',
+      browserVersion: 'latest',
+      os: 'Windows',
+      osVersion: '11'
+    }
+  },
+  {
+    browserName: 'Firefox',
+    'wdio-ics:options': {
+      logName: 'win-firefox'
+    },
+    'bstack:options': {
+      idleTimeout: 300,
+      resolution: '1920x1080',
+      browserVersion: 'latest',
+      os: 'Windows',
+      osVersion: '11'
+    }
+  },
   // macOS
-  // {
-  //   browserName: 'Chrome',
-  //   'wdio-ics:options': {
-  //     logName: 'osx-chrome'
-  //   },
-  //   'bstack:options': {
-  //     idleTimeout: 300,
-  //     resolution: '1920x1080',
-  //     browserVersion: 'latest',
-  //     os: 'OS X',
-  //     osVersion: 'Sonoma' // Changed from 'Sequoia'
-  //   }
-  // }
+  {
+    browserName: 'Chrome',
+    'wdio-ics:options': {
+      logName: 'osx-chrome'
+    },
+    'bstack:options': {
+      idleTimeout: 300,
+      resolution: '1920x1080',
+      browserVersion: 'latest',
+      os: 'OS X',
+      osVersion: 'Sequoia' // Updated from 'Sonoma'
+    }
+  },
+  {
+    browserName: 'Safari',
+    'wdio-ics:options': {
+      logName: 'osx-safari'
+    },
+    'bstack:options': {
+      idleTimeout: 300,
+      resolution: '1920x1080',
+      browserVersion: 'latest',
+      os: 'OS X',
+      osVersion: 'Sequoia'
+    }
+  },
+  {
+    browserName: 'Firefox',
+    'wdio-ics:options': {
+      logName: 'osx-firefox'
+    },
+    'bstack:options': {
+      idleTimeout: 300,
+      resolution: '1920x1080',
+      browserVersion: 'latest',
+      os: 'OS X',
+      osVersion: 'Sequoia'
+    }
+  },
+  {
+    browserName: 'Safari',
+    'wdio-ics:options': {
+      logName: 'ios-safari'
+    },
+    'bstack:options': {
+      idleTimeout: 300,
+      deviceName: 'iPhone 14',
+      osVersion: '16',
+      realMobile: true,
+      browserVersion: 'latest'
+    }
+  },
+  {
+    browserName: 'Chrome',
+    'wdio-ics:options': {
+      logName: 'ios-chrome'
+    },
+    'bstack:options': {
+      idleTimeout: 300,
+      deviceName: 'iPhone 15',
+      osVersion: '16',
+      realMobile: true,
+      browserVersion: 'latest'
+    }
+  },
+  {
+    browserName: 'Chrome',
+    'wdio-ics:options': {
+      logName: 'android-chrome'
+    },
+    'bstack:options': {
+      idleTimeout: 300,
+      deviceName: 'Samsung Galaxy S23',
+      osVersion: '13.0',
+      realMobile: true,
+      browserVersion: 'latest'
+    }
+  },
+  {
+    browserName: 'Samsung',
+    'wdio-ics:options': {
+      logName: 'android-samsung-internet'
+    },
+    'bstack:options': {
+      idleTimeout: 300,
+      deviceName: 'Samsung Galaxy S23',
+      osVersion: '13.0',
+      realMobile: true,
+      browserVersion: 'latest'
+    }
+  }
 ]
