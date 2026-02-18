1baeaf0c4836169e94d9e8050f7c28878ca7f4c4
Mayank Solanki <manky18@gmail.com>
2026-02-18 09:47:52 +0000

    Update the address page layout changes

--- test/specs/non-prod/contact_details_validation.js
+++ test/specs/non-prod/contact_details_validation.js
@@ -23,13 +23,23 @@ describe('Contact Details validation for a SBI @cdp @dev', () => {
       await ConfirmYourDetailsPage.getSummaryValue('Business name')
     ).toContain('Rose Farm')
     await expect(
-      await ConfirmYourDetailsPage.getSummaryValue('Address')
-    ).toContain(
-      '12 Green Lane\n' + 'Trading Estate\n' + 'Birmingham\n' + 'ZE60 6BA'
-    )
+      await ConfirmYourDetailsPage.getSummaryValue('Address 1')
+    ).toContain('12 Green Lane')
     await expect(
-      await ConfirmYourDetailsPage.getSummaryValue('Contact details')
-    ).toContain('07300 000011\n' + 'contact+300000011@example.test')
+      await ConfirmYourDetailsPage.getSummaryValue('Address 2')
+    ).toContain('Trading Estate')
+    await expect(
+      await ConfirmYourDetailsPage.getSummaryValue('City')
+    ).toContain('Birmingham')
+    await expect(
+      await ConfirmYourDetailsPage.getSummaryValue('Postcode')
+    ).toContain('ZE60 6BA')
+    await expect(
+      await ConfirmYourDetailsPage.getSummaryValue('Mobile number')
+    ).toContain('07300 000011')
+    await expect(
+      await ConfirmYourDetailsPage.getSummaryValue('Email address')
+    ).toContain('contact+300000011@example.test')
   })
 
   it('SBI 300000001 - Structured contact details are shown correctly', async () => {
@@ -44,11 +54,20 @@ describe('Contact Details validation for a SBI @cdp @dev', () => {
       await ConfirmYourDetailsPage.getSummaryValue('Business name')
     ).toContain('Green Farm')
     await expect(
-      await ConfirmYourDetailsPage.getSummaryValue('Address')
-    ).toContain('York Park House\n' + 'Maidstone\n' + 'W50 5UY')
+      await ConfirmYourDetailsPage.getSummaryValue('Address 1')
+    ).toContain('York Park House')
     await expect(
-      await ConfirmYourDetailsPage.getSummaryValue('Contact details')
-    ).toContain('07300 000001\n' + 'contact+300000001@example.test')
+      await ConfirmYourDetailsPage.getSummaryValue('City')
+    ).toContain('Maidstone')
+    await expect(
+      await ConfirmYourDetailsPage.getSummaryValue('Postcode')
+    ).toContain('W50 5UY')
+    await expect(
+      await ConfirmYourDetailsPage.getSummaryValue('Mobile number')
+    ).toContain('07300 000001')
+    await expect(
+      await ConfirmYourDetailsPage.getSummaryValue('Email address')
+    ).toContain('contact+300000001@example.test')
   })
 
   it('SBI 106440951 - Structured contact details are shown correctly', async () => {
@@ -63,17 +82,25 @@ describe('Contact Details validation for a SBI @cdp @dev', () => {
       await ConfirmYourDetailsPage.getSummaryValue('Business name')
     ).toContain('Alport Farm')
     await expect(
-      await ConfirmYourDetailsPage.getSummaryValue('Address')
-    ).toContain(
-      'Top Farm\n' +
-        '04 Holloway Saxby-All-Saints Orford Road\n' +
-        'Havering\n' +
-        'Redbridge\n' +
-        'Yeovil\n' +
-        'YO25 3PF'
-    )
+      await ConfirmYourDetailsPage.getSummaryValue('Address 1')
+    ).toContain('Top Farm')
     await expect(
-      await ConfirmYourDetailsPage.getSummaryValue('Contact details')
+      await ConfirmYourDetailsPage.getSummaryValue('Address 2')
+    ).toContain('04 Holloway Saxby-All-Saints Orford Road')
+    await expect(
+      await ConfirmYourDetailsPage.getSummaryValue('Address 3')
+    ).toContain('Havering')
+    await expect(
+      await ConfirmYourDetailsPage.getSummaryValue('Address 4')
+    ).toContain('Redbridge')
+    await expect(
+      await ConfirmYourDetailsPage.getSummaryValue('City')
+    ).toContain('Yeovil')
+    await expect(
+      await ConfirmYourDetailsPage.getSummaryValue('Postcode')
+    ).toContain('YO25 3PF')
+    await expect(
+      await ConfirmYourDetailsPage.getSummaryValue('Email address')
     ).toContain('alportfarmi@mraftroplax.com.test')
   })
 
@@ -89,8 +116,14 @@ describe('Contact Details validation for a SBI @cdp @dev', () => {
       await ConfirmYourDetailsPage.getSummaryValue('Business name')
     ).toContain('MA SCOTT & SON')
     await expect(
-      await ConfirmYourDetailsPage.getSummaryValue('Address')
-    ).toContain('Nirvana HydeWay\n' + 'WESTERHAM\n' + 'LE4 0SR')
+      await ConfirmYourDetailsPage.getSummaryValue('Address 1')
+    ).toContain('Nirvana HydeWay')
+    await expect(
+      await ConfirmYourDetailsPage.getSummaryValue('City')
+    ).toContain('WESTERHAM')
+    await expect(
+      await ConfirmYourDetailsPage.getSummaryValue('Postcode')
+    ).toContain('LE4 0SR')
     await expect(ConfirmYourDetailsPage.contactDetailsKey).not.toBeExisting()
   })
 })
