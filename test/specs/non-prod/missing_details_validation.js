da63878642ab76601054d4bcdab56a89c12369dc
Mayank Solanki <manky18@gmail.com>
2026-02-19 16:53:43 +0000

    evert back to old way of clearing application state using URL
    As the new API way fails whilst running the tests on CI

--- test/specs/non-prod/missing_details_validation.js
+++ test/specs/non-prod/missing_details_validation.js
@@ -14,11 +14,11 @@ describe('Missing Personal Details or Business details for a Farmer @cdp @dev', 
   describe('SBI 400000008 - Title for Farmer is missing', async () => {
     it('Then farmer is shown validation message and not allowed to complete the application', async () => {
       const crn = '1400000008'
-      const sbi = '400000008'
+      // const sbi = '400000008'
 
       await HomePage.open()
       await LoginPage.login(crn)
-      await HomePage.clearApplicationStateWithApi(crn, sbi)
+      await HomePage.clearApplicationState()
 
       await ConfirmYourDetailsPage.validateMissingDetailsWarning()
       await ConfirmYourDetailsPage.validateValueEmptyFor('Title')
@@ -29,11 +29,11 @@ describe('Missing Personal Details or Business details for a Farmer @cdp @dev', 
   describe('SBI 400000006 - Last name for Farmer is missing', async () => {
     it('Then farmer is shown validation message and not allowed to complete the application', async () => {
       const crn = '1400000006'
-      const sbi = '400000006'
+      // const sbi = '400000006'
 
       await HomePage.open()
       await LoginPage.login(crn)
-      await HomePage.clearApplicationStateWithApi(crn, sbi)
+      await HomePage.clearApplicationState()
 
       await ConfirmYourDetailsPage.validateMissingDetailsWarning()
       await ConfirmYourDetailsPage.validateValueEmptyFor('Last name')
@@ -44,11 +44,11 @@ describe('Missing Personal Details or Business details for a Farmer @cdp @dev', 
   describe('SBI 400000002 - Address line 1 for Farmer is missing', async () => {
     it('Then farmer is shown validation message and not allowed to complete the application', async () => {
       const crn = '1400000002'
-      const sbi = '400000002'
+      // const sbi = '400000002'
 
       await HomePage.open()
       await LoginPage.login(crn)
-      await HomePage.clearApplicationStateWithApi(crn, sbi)
+      await HomePage.clearApplicationState()
 
       await ConfirmYourDetailsPage.validateMissingDetailsWarning()
       await ConfirmYourDetailsPage.validateValueEmptyFor('Address 1')
