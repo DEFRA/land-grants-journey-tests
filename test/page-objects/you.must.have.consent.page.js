851422bed0790a9c04b7a1cae79e66977108cd2a
Mayank Solanki <manky18@gmail.com>
2026-02-26 16:27:58 +0000

    add test for sssi and hefer combined scenario

--- test/page-objects/you.must.have.consent.page.js
+++ test/page-objects/you.must.have.consent.page.js
@@ -20,6 +20,25 @@ class YouMustHaveConsentPage extends Page {
       'Read the guidance on land with historic or archaeological features (opens in new tab) to find out what a HEFER is and how to request one.'
     )
   }
+
+  async validateSssiAndHeferConsentContent() {
+    const heading = await $('.govuk-panel__title')
+    await expect(heading).toBeDisplayed()
+    const headingText = (await heading.getText()).trim()
+    const expectedHeading = 'You must get consent to do your actions'
+    await expect(headingText).toContain(expectedHeading)
+
+    const body = await $('.govuk-panel__body')
+    await expect(body).toBeDisplayed()
+    const bodyText = await body.getText()
+    await expect(bodyText).toContain(
+      "You are applying for actions on land that's a site of special scientific interest (SSSI) and land with historic or archaeological features. Before starting your actions, you must get:"
+    )
+    await expect(bodyText).toContain(
+      'an SFI Historic Environment Farm Environment Record (SFI HEFER) (opens in new tab) from Historic England\n' +
+        'SSSI consent (opens in new tab) from Natural England'
+    )
+  }
 }
 
 export default new YouMustHaveConsentPage()
---
+++ test/specs/non-prod/actions_with_sssi_and_hefer_consent.e2e.js
@@ -0,0 +1,97 @@
+import { browser, expect } from '@wdio/globals'
+
+import HomePage from 'page-objects/home.page.js'
+import ConfirmYourDetailsPage from 'page-objects/confirm.your.details.page.js'
+import ConfirmYourLandDetailsPage from 'page-objects/confirm.your.land.details.js'
+import SelectLandParcelsPage from 'page-objects/select.land.parcels.page.js'
+import ActionsPage from 'page-objects/select.actions.page.js'
+import ReviewTheActionsYouHaveSelectedPage from 'page-objects/review.the.actions.page.js'
+import { SERVICE_NAME } from '~/test/utils/config.js'
+import SubmitYourApplicationPage from 'page-objects/submit.your.application.page.js'
+import ConfirmYouWillBeEligiblePage from 'page-objects/confirm.you.will.be.eligible.page'
+import LoginPage from 'page-objects/login.page.js'
+import YouMustHaveConsentPage from 'page-objects/you.must.have.consent.page.js'
+
+describe('Actions that require SSSI and HEFER Consent @cdp @dev', () => {
+  describe('Given farmer is eligible for funding', () => {
+    describe('When farmer goes through the land grants application', () => {
+      const crn = '1101092483'
+      const sbi = '106841262'
+      const parcel = 'ST1437-7349'
+      const action = 'UPL2'
+
+      it('Then the farmer is shown the landing page', async () => {
+        await HomePage.open()
+        await LoginPage.login(crn)
+      })
+
+      it('Then the farmer is shown confirm your details page', async () => {
+        await HomePage.clearApplicationStateWithApi(crn, sbi)
+        await expect(browser).toHaveTitle(
+          `Confirm your details | ${SERVICE_NAME}`
+        )
+      })
+
+      it('Then the farmer is shown the confirm eligibility page', async () => {
+        await ConfirmYourDetailsPage.clickButton('Continue')
+        await expect(browser).toHaveTitle(
+          `Confirm you will be eligible | ${SERVICE_NAME}`
+        )
+      })
+
+      it('Then the farmer is shown the confirm your land details are up to date page', async () => {
+        await ConfirmYouWillBeEligiblePage.clickButton('Continue')
+        await expect(browser).toHaveTitle(
+          `Confirm your land details are up to date | ${SERVICE_NAME}`
+        )
+      })
+
+      it('Then the farmer is shown the list of land parcels page', async () => {
+        await ConfirmYourLandDetailsPage.clickButton('Continue')
+        await expect(browser).toHaveTitle(
+          `Select land parcel for actions | ${SERVICE_NAME}`
+        )
+      })
+
+      it('Then the farmer adds an action to the land parcel', async () => {
+        await SelectLandParcelsPage.selectRequiredLandParcel(parcel)
+        await SelectLandParcelsPage.clickButton('Continue')
+        await expect(browser).toHaveTitle(
+          `Select actions for land parcel ${parcel.replace('-', ' ')} | ${SERVICE_NAME}`
+        )
+        await expect(await ActionsPage.getSssiAndHeferMessage()).toContain(
+          'You must have SSSI consent (opens in new tab) and get a HEFER (opens in new tab) to do these actions on this land parcel.'
+        )
+        await ActionsPage.selectRequiredAction(action)
+        await SelectLandParcelsPage.clickButton('Continue')
+        await expect(browser).toHaveTitle(
+          `Review the actions you have selected | ${SERVICE_NAME}`
+        )
+      })
+
+      it('Then the farmer says No to add another action to the land parcel, is shown SSSI & HEFER consent page', async () => {
+        await ReviewTheActionsYouHaveSelectedPage.doYouWantToAddAnotherAction(
+          'false'
+        )
+        await ReviewTheActionsYouHaveSelectedPage.clickButton('Continue')
+        await expect(browser).toHaveTitle(
+          `You must have consent | ${SERVICE_NAME}`
+        )
+        await YouMustHaveConsentPage.validateSssiAndHeferConsentContent()
+        await YouMustHaveConsentPage.clickButton('Continue')
+      })
+
+      it('Then the farmer is shown the submit your application page', async () => {
+        await expect(browser).toHaveTitle(
+          `Submit your application | ${SERVICE_NAME}`
+        )
+        const submitButton = await SubmitYourApplicationPage.submitButton()
+        await submitButton.click()
+      })
+
+      it('Then the farmer is shown the confirmation page', async () => {
+        await expect(browser).toHaveTitle(`Confirmation | ${SERVICE_NAME}`)
+      })
+    })
+  })
+})
